f2eb0f4c6945f9bab39d2e064d6b7e93eb9a8fc6
thaichor <thaichor@gmail.com>
2019-04-23 17:27:44 +0700

    finish week 2

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,9 @@
 class Teacher < ApplicationRecord
+  enum gender: %i[female male]
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :date_of_birth, presence: true
 end
--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Teacher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:phone) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:date_of_birth) }
+  end
 end
