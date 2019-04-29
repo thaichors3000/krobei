5755543aab5e797d6e425563974a852a55b211e8
NironHul <nironhul855@gmail.com>
2019-04-29 14:13:42 +0700

    niron week2

---
+++ app/models/student.rb
@@ -0,0 +1,9 @@
+class Student < ApplicationRecord
+  enum gender: %i[female male]
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :date_of_birth, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :students
   root to: 'home#index'
   
   resources :teachers
+  resources :students
 end
