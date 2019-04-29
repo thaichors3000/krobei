f635ddb5971a60ff9ac19557516681a737ce88c6
thy.bunnarith <thy.bunnarith@gmail.com>
2019-04-29 15:12:00 +0700

    [Finish #Studen]: bunnarith Create table student
    Why: edit update delect creat and style bootstrap
    
    * ...
    
    This change addresses the need by:
    
    * ...

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_102915) do
+ActiveRecord::Schema.define(version: 2019_04_29_065535) do
 
-  create_table "teachers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "students", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "phone", null: false
     t.string "email", null: false
     t.integer "gender", default: 0
     t.date "date_of_birth", null: false
+  end
+
+  create_table "teachers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.string "email"
+    t.integer "gender"
+    t.date "date_of_birth"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
