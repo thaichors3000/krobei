b7b38278ccdb3af4822d6e91274f8f8eb8d122d9
Kimsan Lim <kimsanlim27@gmail.com>
2019-05-06 00:12:36 +0700

    Finish homework week3

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,9 @@ Student.create([
   { first_name: 'Cheang', last_name: 'Veang', email: 'kimsrung@gmail.com', phone: '016222111', parents_phone: '016222333', date_of_birth: '2019-01-01' },
   { first_name: 'Sinh', last_name: 'Steve', email: 'bunthai@gmail.com', phone: '016223111', parents_phone: '016222333', date_of_birth: '2019-01-01' }
 ])
+
+Course.create([
+  { name: 'Ruby on Rails', start_date: '2019-04-01', end_date: '2019-07-01', teacher_id: 1 },
+  { name: 'React Native', start_date: '2019-02-10', end_date: '2019-09-10', teacher_id: 2 },
+  { name: 'C++', start_date: '2019-01-01', end_date: '2019-06-01', teacher_id: 3 }
+])
