289c424512082d31d11525215abc7cb02d1c87f1
chanvuthea <deochanvuthea@gmail.com>
2019-05-28 10:04:36 +0700

    vuthea w5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope '(:locale)' do
+  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/, defaults: {locale: "en"} do
     root to: 'home#index'
 
     resources :courses
